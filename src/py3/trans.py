e28237d2b40083900a6166948e31571d1725323a
Apua <Apua.A.Aa@gmail.com>
2020-12-04 10:47:15 +0800

    tmp

---
+++ src/py3/trans.py
@@ -0,0 +1,30 @@
+readers/__init__.py::Reader.get_transforms
+
+    ('820-001', <class 'docutils.transforms.universal.Decorations'>, None, {}),
+    ('840-002', <class 'docutils.transforms.universal.ExposeInternals'>, None, {}),
+    ('740-003', <class 'docutils.transforms.universal.StripComments'>, None, {}),
+
+readers/standalone.py::Reader.get_transforms
+
+    ('220-004', <class 'docutils.transforms.references.Substitutions'>, None, {}),
+    ('260-005', <class 'docutils.transforms.references.PropagateTargets'>, None, {}),
+    ('320-006', <class 'docutils.transforms.frontmatter.DocTitle'>, None, {}),
+    ('350-007', <class 'docutils.transforms.frontmatter.SectionSubTitle'>, None, {}),
+    ('340-008', <class 'docutils.transforms.frontmatter.DocInfo'>, None, {}),
+    ('440-009', <class 'docutils.transforms.references.AnonymousHyperlinks'>, None, {}),
+    ('460-010', <class 'docutils.transforms.references.IndirectHyperlinks'>, None, {}),
+    ('620-011', <class 'docutils.transforms.references.Footnotes'>, None, {}),
+    ('640-012', <class 'docutils.transforms.references.ExternalTargets'>, None, {}),
+    ('660-013', <class 'docutils.transforms.references.InternalTargets'>, None, {}),
+    ('850-014', <class 'docutils.transforms.references.DanglingReferences'>, None, {}),
+    ('830-015', <class 'docutils.transforms.misc.Transitions'>, None, {}),
+
+parsers/rst/__init__.py::Parser.get_transforms
+
+    ('850-016', <class 'docutils.transforms.universal.SmartQuotes'>, None, {}),
+
+writers/__init__.py::Writer.get_transforms
+
+    ('860-017', <class 'docutils.transforms.universal.Messages'>, None, {}),
+    ('870-018', <class 'docutils.transforms.universal.FilterMessages'>, None, {}),
+    ('420-019', <class 'docutils.transforms.universal.StripClassesAndElements'>, None, {}),
